e2f88f62177bf880a9cf676d8b0d595e802d21e8
MarnissiAbdelkarim <karimmernissi101@gmail.com>
2023-10-14 13:16:08 +0100

    project structure

---
+++ src/main/java/com/hcl/abclibrarymanagementsystem/AbcLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.abclibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcLibraryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcl/abclibrarymanagementsystem/AbcLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.abclibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcLibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
